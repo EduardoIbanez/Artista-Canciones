f27e9f392e0f3501a04e95b10c7f9bfb245be9f5
CristobalDelaMaza <c.delamaza01@ufromail.cl>
2018-12-11 17:23:53 -0300

    Borrar y paguinas con fragment

--- src/main/java/dci/ufro/cl/artistascanciones/controller/ArtistaController.java
+++ src/main/java/dci/ufro/cl/artistascanciones/controller/ArtistaController.java
@@ -7,10 +7,7 @@ import dci.ufro.cl.artistascanciones.model.data.EstiloDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -24,32 +21,39 @@ public class ArtistaController {
     @Autowired
     private EstiloDAO estiloDAO;
 
-   @RequestMapping(value = "")
+   @RequestMapping(value = "")// muesta lista de artistas
     public String index(Model modelo) {
 
-        modelo.addAttribute("tituloTabla", "Artistas");
-        modelo.addAttribute("artistas", artistaDAO.findAll());
-        return "artista/index";
+       modelo.addAttribute("artistas", artistaDAO.findAll()); //carga artistas
+       modelo.addAttribute(new Artista()); // guarda datos para nuevo artista submit
+       modelo.addAttribute("estilos", estiloDAO.findAll()); // carga estilos
+       return "artista/index";
     }
 
-    @RequestMapping(value = "add", method = RequestMethod.GET)
-    public String mostrarFormularioArtista(Model modelo) {
 
-        modelo.addAttribute("tituloFormulario", "Ingreso de Artista");
-        modelo.addAttribute(new Artista());
-        modelo.addAttribute("estilos", estiloDAO.findAll());
-
-        return "artista/add";
-    }
-
-    @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String procesarFormularioPersona(@ModelAttribute Artista a,
-                                            @RequestParam int estilo,
-                                            Model model) {
+    @RequestMapping(value = "", method = RequestMethod.POST) // agregar artistas
+    public String agregarArtistas(@ModelAttribute Artista a,
+                                            @RequestParam int estilo) {
 
         Optional<Estilo> est = estiloDAO.findById(estilo);
         a.setEstilo(est.get());
         artistaDAO.save(a);
-        return "redirect:";
+        return "redirect:/artista";
     }
+
+    @RequestMapping(value = "borrar/{id}", method = RequestMethod.GET)
+    public String eliminarArtista(@PathVariable("id") int id){
+
+        artistaDAO.deleteById(id);
+        return "redirect:/artista";
+    }
+//
+//    @RequestMapping(value = "editar/{id}", method = RequestMethod.)
+//    public String editarArtista(){
+//
+//
+//       return "redirect:/artista";
+//    }
+
+
 }
